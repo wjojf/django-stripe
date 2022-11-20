110c3106f66dea903cfbbeb8874a1c2ed462d49c
wjojf <STPKBFF@yandex.ru>
2022-11-20 17:22:42 +0500

    Add create_instances command

---
+++ app/core/management/commands/create_instances.py
@@ -0,0 +1,71 @@
+from django.core.management.base import BaseCommand
+from core.models import Item, Price, Currency, Order
+
+
+class Command(BaseCommand):
+
+    ITEMS = [
+        {
+            "name": "Item 1",
+            "description": "Description of Item 1",
+            "price": {
+                "unit_price": 500,
+                "currency": {
+                    "name": "usd",
+                    "symbol": "$"
+                }
+            }
+        },
+        {
+            "name": "Item 2",
+            "description": "Description of Item 2",
+            "price": {
+                "unit_price": 250,
+                "currency": {
+                    "name": "usd",
+                    "symbol": "$"
+                }
+            }
+        }
+    ]
+
+    def create_test_items(self):
+        output = {}
+
+        for item in self.ITEMS:
+            currency_obj, currency_created = Currency.objects.get_or_create(
+                name=item["price"]["currency"]["name"],
+                symbol=item["price"]["currency"]["symbol"]
+            )
+            price_obj, price_created= Price.objects.get_or_create(
+                unit_price=item["price"]["unit_price"],
+                currency=currency_obj
+            )
+            item_obj, item_created = Item.objects.get_or_create(
+                name=item["name"],
+                description=item["description"],
+                price=price_obj
+            )
+
+            if item_created:
+                output[item_obj.id] = item_obj
+        
+        return output
+
+
+    def create_test_order(self, order_items:dict):
+        if not order_items:
+            return 
+        
+        order_obj, order_created = Order.objects.get_or_create(
+            items=list(order_items.keys())
+        )
+
+        if order_created:
+            print(f"Sucessfully created order {order_obj}")
+        
+
+    def handle(self, *args, **options):
+        print('[LOG] -> Initializing test database...')
+        self.create_test_order(self.create_test_items())
+        print('[LOG] -> Finished initializing test database')
